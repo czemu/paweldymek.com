3986005a6d94ba3c308e33bf04df3b1a562cb5fe
Paweł Dymek <pawel.dymek@gmail.com>
2020-01-24 22:53:15 +0100

    Add post filtering by locale in RSS feed

--- app/Http/Controllers/RssController.php
+++ app/Http/Controllers/RssController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App;
 
 class RssController extends Controller
 {
     public function posts()
     {
-        $posts = Post::published()->orderBy('id', 'DESC')->take(20)->get();
+        $posts = Post::locale(App::getLocale())
+            ->published()
+            ->orderBy('id', 'DESC')
+            ->take(20)
+            ->get();
 
         return response(view('rss.posts', compact('posts')), 200, [
             'Content-Type' => 'application/xml'
