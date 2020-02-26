ce15dedaea067e0c810ef594fe32d9cd7bdafd90
Paweł Dymek <pawel.dymek@gmail.com>
2020-02-26 20:32:20 +0100

    Remove button from tag index

--- app/Nova/Tag.php
+++ app/Nova/Tag.php
@@ -9,7 +9,6 @@ use Benjaminhirsch\NovaSlugField\TextWithSlug;
 use Laravel\Nova\Fields\BelongsTo;
 use Illuminate\Http\Request;
 use Laravel\Nova\Http\Requests\NovaRequest;
-use NovaButton\Button;
 
 class Tag extends Resource
 {
@@ -77,7 +76,6 @@ class Tag extends Resource
                ->creationRules('unique:tags,slug')
                ->updateRules('unique:tags,slug,{{resourceId}}')
                ->hideFromIndex(),
-           Button::make(__('Show'))->link($this->getUrl())->style('primary-outline'),
         ];
     }
 
