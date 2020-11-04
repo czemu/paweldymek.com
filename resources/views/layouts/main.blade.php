236b0c575474a3effdbde4ed5fd3a999ff721c5d
Paweł Dymek <pawel.dymek@gmail.com>
2020-11-04 19:28:21 +0100

    Update favicons and manifest.json

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -25,6 +25,9 @@
         <link rel="icon" type="image/png" sizes="32x32" href="{{ url('favicon-32x32.png') }}">
         <link rel="icon" type="image/png" sizes="16x16" href="{{ url('favicon-16x16.png') }}">
         <link rel="manifest" href="{{ url('manifest.json') }}">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="theme-color" content="#ffffff">
+
         <link rel="alternate" type="application/rss+xml" title="RSS" href="{{ route('rss.posts') }}" />
 
         @hasSection('open_graph')
