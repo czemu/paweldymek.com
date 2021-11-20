b2846f7ca6c570351971e7740c9b131b65b760d4
Paweł Dymek <pawel.dymek@gmail.com>
2021-11-20 12:05:18 +0100

    Add migrations for laravel-medialibrary v8

---
+++ database/migrations/2021_11_20_115804_add_uuid_field_to_media_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class AddUuidFieldToMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('media', function (Blueprint $table) {
+            $table->char('uuid', 36)->after('id');
+        });
+
+
+        Media::cursor()->each(
+            fn (Media $media) => $media->update(['uuid' => Str::uuid()])
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('media', function (Blueprint $table) {
+            $table->dropColumn('uuid');
+        });
+    }
+}
